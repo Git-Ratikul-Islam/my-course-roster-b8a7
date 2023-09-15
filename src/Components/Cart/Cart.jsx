59910bcdef9e219a4f43b290efc4d2b1453ded1a
Ratikulislam <joyofficial80@gmail.com>
2023-09-15 12:50:20 +0600

    if-card-is-clicked-twice-show-alert/toast

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,6 +1,35 @@
-const Cart = () => {
+
+const Cart = ({ selectedCourse }) => {
+      console.log(selectedCourse);
+
       return (
-            
+            <div className='w-1/6 sm:items-center '>
+                  <div className='card p-5 bg-base-100 shadow-xl mt-5 pb-40' >
+                        <h1 className=' text-[#2F80ED] font-semibold text-start'>Credit Hour Remaining  hr {selectedCourse.length}  <hr className="h-px w-66 my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr></h1>
+                        <div>
+                              <h1 className='text-xl font-bold'>
+                                    Course Name
+                              </h1>
+                              {
+                                    selectedCourse.map(course => (
+                                          <ol key={course.id} className="list-decima"><li>{course.title}</li></ol>
+                                    ))
+                              }
+
+
+                              <hr className="h-px w-66 my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+                        </div>
+
+                        {selectedCourse.map(course => (
+                              <h1 key={course.id}>Total Credit Hour : {course.credit}
+                              </h1>
+
+                        ))}
+                        <hr className="h-px w-66 my-2 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+
+
+                  </div>
+            </div>
       );
 };
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,23 +1,47 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-key */
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDollarSign, faBookOpen } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 
 
 const Home = () => {
       const [course, setCourse] = useState([]);
+      const [selectedCourse, setSelectedCourse] = useState([]);
       useEffect(() => {
             fetch('course.json')
                   .then(res => res.json())
                   .then(data => setCourse(data));
       }, []);
-      console.log(course);
+      // console.log(course);
 
+
+
+      const haldleSelectCourse = (course) => {
+
+            const isExist = selectedCourse.find((item) => item.id == course.id);
+            let count = course.credit;
+
+            if (isExist) {
+                  alert("selected");
+            }
+            else {
+                  selectedCourse.forEach((course) => {
+                        count = count + course.credit;
+                  });
+                  console.log(count);
+
+                  setSelectedCourse([...selectedCourse, course]);
+            }
+      };
+      // console.log(selectedCourse);
 
       return <>
+            <h1 className='text-4xl text-center m-8  font-bold'>Course Registration</h1>
             <div className='lg:flex'>
-                  <div className='w-5/6 gap-3 mx-10 flex flex-wrap'>
+                  <div className='w-4/6 gap-3 mx-10 flex flex-wrap'>
                         {
                               course.map(course => (
                                     <div key={course.id} className="card w-96 bg-base-100 shadow-xl ml-6">
@@ -32,7 +56,7 @@ const Home = () => {
                                                       <h4><FontAwesomeIcon icon={faBookOpen} /> Credit: {course.credit}</h4>
                                                 </div>
                                                 <div className="card-actions mx-auto ">
-                                                      <button className="btn btn-primary px-[105px] text-white bg-[#2F80ED] border-none">Buy Now</button>
+                                                      <button onClick={() => haldleSelectCourse(course)} className="btn btn-primary px-[110px] text-white bg-[#2F80ED] border-none">Select</button>
                                                 </div>
                                           </div>
 
@@ -40,9 +64,7 @@ const Home = () => {
                               ))
                         }
                   </div>
-                  <div className='w-1/6 ' >
-                        <h1 className='text-4xl'>card</h1>
-                  </div>
+                  <Cart selectedCourse={selectedCourse}></Cart>
 
             </div>
 
